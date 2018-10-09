d6d554c20f4b77fa77ceb1f27d6e8bb01b309017
Josh Zimmerman <JoshuaZimmerman@quickenloans.com>
2018-10-09 13:14:36 -0400

    This is my initial commit for Dev.Build Lab 2 - Classrom Area and Perimeter

---
+++ DevBuild.GrandCircusClassrooms/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevBuild.GrandCircusClassrooms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RectangularRoom roomObject;
+            double tmpLength, tmpWidth;
+            string userResponse = "";
+            bool userValidResponse = false;
+            bool userExit = false;
+
+            Console.Write("***********************************************************\n" +
+                           "*       Welcome to Dev.Build(2.0) Room Calculator!       *\n" +
+                           "**********************************************************\n\n");
+
+            while (!userExit)
+            {
+                roomObject = new RectangularRoom();
+                userValidResponse = false;
+                Console.Write("Enter Room Length, in feet: ");
+                if (double.TryParse(Console.ReadLine(), out tmpLength))
+                {
+                    roomObject.Length = tmpLength;
+                }
+
+                Console.Write("Enter Room Width, in feet: ");
+                if (double.TryParse(Console.ReadLine(), out tmpWidth))
+                {
+                    roomObject.Width = tmpWidth;
+                }
+
+                Console.WriteLine("\nArea is {0} sq. ft.", roomObject.Area);
+                Console.WriteLine("Perimeter = {0} ft.\n", roomObject.Perimeter);
+
+                while (!userValidResponse)
+                {
+                    Console.WriteLine("Enter dimensions for another room? (type y/n or yes/no)");
+                    userResponse = Console.ReadLine();
+                    switch (userResponse.ToLower())
+                    {
+                        case "y":
+                        case "yes":
+                            {
+                                //we received a valid response from the user, and user wants to enter another set of dimensions
+                                //let's make sure we stay in the program loop by keeping userExit false, and reset to the top of the loop
+                                userValidResponse = true;
+                                userExit = false;
+                               
+                                break;
+                            }
+
+                        case "n":
+                        case "no":
+                            {
+                                userValidResponse = true;
+                                userExit = true;
+                                break;
+                            }
+
+                        default:
+                            {
+                                userValidResponse = false;
+                                Console.WriteLine("I didn't recognize your response. Please enter y/n or yes/no)");
+                                break;
+                            }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ DevBuild.GrandCircusClassrooms/RectangularRoom.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevBuild.GrandCircusClassrooms
+{
+    class RectangularRoom
+    {
+        public double Width { get; set; } = 0.0;
+        public double Length { get; set; } = 0.0;
+        public double Area { get { return Width * Length; } }
+        public double Perimeter { get { return (2f * Width) + (2f * Length); } }
+
+        public RectangularRoom()
+        {
+
+        }
+
+    }
+}
